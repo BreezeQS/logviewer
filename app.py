a2083800a40be9218c663d9b5f312123ce341c2a
BreezeQS <70638053+BreezeQS@users.noreply.github.com>
2021-03-25 14:53:53 +0300

    This probably won't work

--- app.py
+++ app.py
@@ -81,6 +81,6 @@ async def get_logs_file(request, document):
 if __name__ == "__main__":
     app.run(
         host=os.getenv("HOST", "0.0.0.0"),
-        port=os.getenv("PORT", 8000),
+        port=443,
         debug=bool(os.getenv("DEBUG", False)),
     )
